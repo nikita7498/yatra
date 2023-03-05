502a200ad5ff9cd0645288be2c6e8e288020a412
Nikita <nilkanthnikita24@gmail.com>
2023-03-05 09:11:44 +0530

    codepush

---
+++ src/main/java/com/example/yatra/Flight.java
@@ -0,0 +1,13 @@
+package com.example.yatra;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class Flight {
+    @GetMapping("/flight")
+    public String getData(){
+        return  "book my ticket";
+    }
+}
